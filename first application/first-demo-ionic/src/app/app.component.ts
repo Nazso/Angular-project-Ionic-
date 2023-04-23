f791601608b23bef112bf866ea3f020bdb7eb615
Nazso <zsolt.nagy.1976@gmail.com>
2023-04-23 13:12:31 +0200

    developed logic

--- first application/first-demo-ionic/src/app/app.component.ts
+++ first application/first-demo-ionic/src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component, EnvironmentInjector, inject } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule],
+  imports: [IonicModule, CommonModule, NgFor],
 })
 export class AppComponent {
   public environmentInjector = inject(EnvironmentInjector);
--- first application/first-demo-ionic/src/app/tab1/tab1.page.ts
+++ first application/first-demo-ionic/src/app/tab1/tab1.page.ts
@@ -1,15 +1,41 @@
 import { Component } from '@angular/core';
 import { IonicModule, createAnimation } from '@ionic/angular';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { CommonModule, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonicModule, ExploreContainerComponent],
+  imports: [IonicModule, ExploreContainerComponent, CommonModule, NgFor],
 })
 export class Tab1Page {
+
+  public cardContents = [
+    {
+      id: 1,
+      header: "Card title One",
+      subHeader: "Card subtitle",
+      message: "Here's a small text description for the card content and try to use ngFor. Nothing more, nothing less."
+    },
+    {
+      id: 2,
+      header: "Card title Two",
+      subHeader: "Card subtitle",
+      message: "Here's a small text description for the second card content and try to use ngFor. Nothing more, nothing less."
+    },
+    {
+      id: 3,
+      header: "Card title Three",
+      subHeader: "Card subtitle",
+      message: "Here's a small text description for the rhird card content and try to use ngFor. Nothing more, nothing less."
+    }
+  ];
+
+  // trackItems(index: number, itemObject: any) {
+  //   return itemObject.id;
+  // }
 
   animationCtrl: any;
   constructor() {}
