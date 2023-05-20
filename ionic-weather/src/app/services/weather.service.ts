a481ee80c5a464e77d2bc518444409c30bd425d8
Nazso <zsolt.nagy.1976@gmail.com>
2023-05-20 19:07:37 +0200

    developed API

--- ionic-weather/src/app/services/weather.service.ts
+++ ionic-weather/src/app/services/weather.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getWeatherData(): Observable<any> {
+    let lat = '37.84';
+    let lon = '144.94';
+    let apiId = '6910680d9e0a588865023bca7bb32739';
+    let queryString = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiId}`;
+
+    return this.http.get(queryString)
+    
+  }
+
 }
