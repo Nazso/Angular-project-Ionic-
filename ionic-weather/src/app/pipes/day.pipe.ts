0746f401219756a316c478a08da5cb0783200007
Nazso <zsolt.nagy.1976@gmail.com>
2023-05-24 17:28:47 +0200

    new pipe

---
+++ ionic-weather/src/app/pipes/day.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'day'
+})
+export class DayPipe implements PipeTransform {
+
+  transform(value: unknown, ...args: unknown[]): unknown {
+    return null;
+  }
+
+}
