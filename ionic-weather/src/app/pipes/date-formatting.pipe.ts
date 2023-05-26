ad17eaa2abcb06066140fa5f881e33816a219329
Nazso <zsolt.nagy.1976@gmail.com>
2023-05-26 17:22:03 +0200

    new pipe implemented

--- ionic-weather/src/app/pipes/date-formatting.pipe.ts
+++ ionic-weather/src/app/pipes/date-formatting.pipe.ts
@@ -5,8 +5,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DateFormattingPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: string): unknown {
+
+    let val = new Date(+value * 1000);
+
+    let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    let days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    return days[val.getDay()] + ', ' + val.getDate() + ' ' + months[val.getMonth()] + ' ' + val.getHours() + ':' + val.getMinutes();
   }
 
 }
